6e79344d9210e7b46b5012cfab1f4a05d14b8185
Diego Souza <souza.claudiodiego@gmail.com>
2024-04-02 17:14:16 -0300

    unpack.sh script

---
+++ unpack.sh
@@ -0,0 +1,19 @@
+cd sanfranciscocabs/f1_raw_data/
+tar -xf sanfranciscocabs_part_1.tar.xz
+tar -xf sanfranciscocabs_part_2.tar.xz
+rm sanfranciscocabs_part_1.tar.xz
+rm sanfranciscocabs_part_2.tar.xz
+
+cd ../../romataxi/f1_raw_data/
+tar -xf romataxi_part_1.tar.xz
+tar -xf romataxi_part_2.tar.xz
+tar -xf romataxi_part_3.tar.xz
+tar -xf romataxi_part_4.tar.xz
+rm romataxi_part_1.tar.xz
+rm romataxi_part_2.tar.xz
+rm romataxi_part_3.tar.xz
+rm romataxi_part_4.tar.xz
+
+cd ../../ngsim/
+tar -xf us-101.tar.xz
+cd ..
